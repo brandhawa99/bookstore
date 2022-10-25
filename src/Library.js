6d097a86f422a619fd9329e85a3db6c83571d181
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-10-24 22:44:15 -0700

    added bookid prop

--- src/Library.js
+++ src/Library.js
@@ -1,6 +1,6 @@
 import Book from "./Book";
 import OpenForm from "./OpenForm";
-function Library({ books }) {
+function Library({ books, user, getBooks }) {
   return (
     <>
       <div className="flex flex-wrap gap-5 mx-3 my-5 justify-center">
@@ -9,11 +9,14 @@ function Library({ books }) {
          * on submit or click on the black part closes the form
          * click on x closes the form
          */}
-        <OpenForm user={books} />
+        <OpenForm user={user} />
         {books &&
           books.map((book, index) => {
+            console.log(book);
             return (
               <Book
+                getBooks={getBooks}
+                user={user}
                 key={index}
                 title={book.title}
                 author={book.author}
